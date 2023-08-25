e684e3d455e52e814da4629b8683880a807512c4
Vaibhav Kumar <142613219+Vaibhav6234@users.noreply.github.com>
2023-08-25 12:04:42 +0530

    codsoft task 2 -  number guessing in c++

---
+++ number guessing.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+int main()
+ {
+
+  int random_number = rand() % 10+1;
+  int guess;
+  cout << "Guess a number between 1 and 10: ";
+  cin >> guess;
+
+
+  while (guess != random_number)
+ {
+    if (guess < random_number)
+        {
+           cout << "Your guess is too low. Try again.\n"<<endl;
+        }
+    else
+        {
+           cout << "Your guess is too high. Try again.\n"<<endl;
+        }
+     cout << "Guess a number between 1 and 10: "<<endl;
+     cin >> guess;
+  }
+
+     cout << "Congratulations! You guessed the correct number.\n"<<endl;
+
+  return 0;
+}
